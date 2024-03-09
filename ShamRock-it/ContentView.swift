745fa30fff7b87fafe8eb4ff423acf3a94152671
Nick Wimmers <91842532+wiimmers@users.noreply.github.com>
2024-03-08 21:04:53 -0500

    Initial Commit

---
+++ ShamRock-it/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShamRock-it
+//
+//  Created by Nicholas Wimmers on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShamRock-it/ShamRock_itApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShamRock_itApp.swift
+//  ShamRock-it
+//
+//  Created by Nicholas Wimmers on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ShamRock_itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
